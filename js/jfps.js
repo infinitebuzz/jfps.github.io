6226978fa1bb63580c10d7af54d34cffd41825e8
James Brooks <james@localhost.localdomain>
2019-03-23 00:15:26 -0400

    still no texture joy

--- js/jfps.js
+++ js/jfps.js
@@ -2,18 +2,21 @@
 https://github.com/infinitebuzz/jfps.github.io
 */
 
-function loadTexture(szTextureAddress, nWidth, nHeight) {
+function loadTexture(szTextureAddress) {
 	var img = new Image();
 	img.src = szTextureAddress;
 	var canvas = document.createElement('canvas');
-	canvas.width = nWidth;
-	canvas.height = nHeight;
+	canvas.width = img.width;
+	canvas.height = img.height;
 	var context = canvas.getContext('2d');
-	context.drawImage(img, 0, 0, nWidth, nHeight);
-	return context;
+	context.drawImage(img, 0, 0, canvas.width, canvas.height);
+	return {
+		width : canvas.width,
+		height : canvas.height,
+		texture : context.getImageData(0, 0, canvas.width, canvas.height)
+	};
 }
 
 function getTexturePixel(x, y, texture) {
 	return texture.getImageData(x, y, 1, 2).data;
 }
-
